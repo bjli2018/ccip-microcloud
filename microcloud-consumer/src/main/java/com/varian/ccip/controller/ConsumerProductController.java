242668d7c67989b09c68ce27a88111c2fd328c0a
dkq6686 <Bob.Li@varian.com>
2019-11-08 09:04:56 +0800

    2019-11-08 09:04 提交

--- microcloud-consumer/src/main/java/com/varian/ccip/controller/ConsumerProductController.java
+++ microcloud-consumer/src/main/java/com/varian/ccip/controller/ConsumerProductController.java
@@ -1,6 +1,9 @@
 package com.varian.ccip.controller;
 
 import com.varian.ccip.vo.Product;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,27 +14,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerProductController {
-    public static final String PRODUCT_GET_URL = "http://localhost:9001/prodcut/get/";
-    public static final String PRODUCT_LIST_URL="http://localhost:9001/prodcut/list/";
-    public static final String PRODUCT_ADD_URL = "http://localhost:9001/prodcut/add/";
+    public static final String PRODUCT_GET_URL = "http://localhost:9001/product/get/";
+    public static final String PRODUCT_LIST_URL="http://localhost:9001/product/list/";
+    public static final String PRODUCT_ADD_URL = "http://localhost:9001/product/add/";
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private HttpHeaders httpHeaders;
+
     @RequestMapping("/product/get")
     public Object getProduct(long id) {
-        Product product = restTemplate.getForObject(PRODUCT_GET_URL + id, Product.class);
+        Product product = restTemplate.exchange(PRODUCT_GET_URL + id,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), Product.class).getBody();
         return  product;
     }
 
     @RequestMapping("/product/list")
     public  Object listProduct() {
-        List<Product> list = restTemplate.getForObject(PRODUCT_LIST_URL, List.class);
+        List<Product> list = restTemplate.exchange(PRODUCT_LIST_URL, HttpMethod.GET,new HttpEntity<Object>(httpHeaders), List.class).getBody();
         return  list;
     }
 
     @RequestMapping("/product/add")
     public Object addPorduct(Product product) {
-        Boolean result = restTemplate.postForObject(PRODUCT_ADD_URL, product, Boolean.class);
+        Boolean result = restTemplate.exchange(PRODUCT_ADD_URL, HttpMethod.POST,new HttpEntity<Object>(product,httpHeaders), Boolean.class).getBody();
         return  result;
     }
 }
