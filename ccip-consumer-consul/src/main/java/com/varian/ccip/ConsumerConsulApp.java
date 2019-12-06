b63749832aa99f3ddd93b52e6f2a65cbb70ee298
dkq6686 <Bob.Li@varian.com>
2019-12-06 13:55:49 +0800

    2019-12-06 13:55 提交

---
+++ ccip-consumer-consul/src/main/java/com/varian/ccip/ConsumerConsulApp.java
@@ -0,0 +1,10 @@
+package com.varian.ccip;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerConsulApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerConsulApp.class,args);
+    }
+}
